0fe4e41569d5fc9e6b2583079eedb4011406edb3
AurelienAllenic <aurelien.allenic@gmail.com>
2023-05-24 16:44:06 +0200

    (feat) commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
   apiKey: "AIzaSyBp3pLcS8GnBAFOFVJZW3J3QiNwzaO-D_Q",
   authDomain: "netflix-clone-bc8f1.firebaseapp.com",
@@ -6,3 +10,12 @@ const firebaseConfig = {
   messagingSenderId: "192864064888",
   appId: "1:192864064888:web:8c93f838e208d8f54ee48d"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebaseApp.firestore()
+
+const auth = firebase.auth();
+
+export {auth}
+export default db;
